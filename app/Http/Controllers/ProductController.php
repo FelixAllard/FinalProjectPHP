ebf0b021865196bef5f160bf6990f2bbb9afdb8c
FelixAllard <xilef992@gmail.com>
2024-04-25 19:51:44 -0400

    added a new view just for creating products!

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-
+    public function product_index(){
+        return view('products.index');
+    }
+    public function product_create_index()
+    {
+        return view('products.create');
+    }
 }
